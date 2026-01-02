38b63d2591fc2c407a20ba2008071fbdef77c29f
curduman-miruna <miruna.curduman@endava.com>
2026-01-02 18:08:00 +0200

    Removed exceptions that will not be used

--- backend/app/utils/exceptions/database_exception.py
+++
@@ -1,7 +0,0 @@
-from app.utils.exceptions.base_app_exception import BaseAppException
-
-
-class DatabaseException(BaseAppException):
-    """Raised for general database errors"""
-    def __init__(self, message: str = "Database error"):
-        super().__init__(message, status_code=500)
--- backend/app/utils/exceptions/user_not_found_exception.py
+++
@@ -1,8 +0,0 @@
-from app.utils.exceptions.base_app_exception import BaseAppException
-
-
-class UserNotFoundException(BaseAppException):
-    """Raised when a user is not found"""
-
-    def __init__(self, message: str = "User not found"):
-        super().__init__(message, status_code=404)
